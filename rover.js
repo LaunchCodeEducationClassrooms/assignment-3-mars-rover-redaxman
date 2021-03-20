268239a0ee1009b8fbecff42ecbd5ec732d85000
redaxman <granthampton888@gmail.com>
2021-03-20 21:31:45 +0000

    updated the previous tests I had to better suit the autograder

--- rover.js
+++ rover.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const Command = require('./command.js');
 const Message = require('./message.js');
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const Rover = require('../rover.js');
 const Message = require('../message.js');
 const Command = require('../command.js');
@@ -7,32 +6,31 @@ const Command = require('../command.js');
 describe("Rover class", function (){
   it("constructor sets position and default values for mode and generatorWatts", function () {
   let rover = new Rover(98382);
-  assert.strictEqual(rover.position, 98382);
-  assert.strictEqual(rover.mode, 'NORMAL');
-  assert.strictEqual(rover.generatorWatts, 110)
+  expect(rover.position).toEqual(98382);
+  expect(rover.mode).toEqual('NORMAL');
+  expect(rover.generatorWatts).toEqual(110);
 });
   it("response returned by receiveMessage contains name of message", function (){ 
   let rover = new Rover(98382);
   let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-  let message = new Message('NAME OF THE MESSAGE-TEST 8', commands);
-
+  let message = new Message('Hello', commands);
   let results = rover.receiveMessage(message);
-  assert.strictEqual(results.message, 'NAME OF THE MESSAGE-TEST 8');
+  expect(results.message).toEqual('Hello')
+
 });
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
     let rover = new Rover(98382);
      let commands = [new Command('MODE_CHANGE', 'LOW_POWER')];
     let message = new Message('test 9', commands);
     let response = rover.receiveMessage(message);
- 
-   assert.strictEqual(response.results.length, commands.length);
+   expect(response.results.length).toEqual(commands.length)
   });
   it("responds correctly to status check command", function () {
     let rover = new Rover(98382);
     let commands = [new Command('STATUS_CHECK')];
     let message = new Message('NAME OF THE MESSAGE', commands);
     let actual = rover.receiveMessage(message).results[0];
-    let expected = {
+    let expectedReturn = {
         completed: true,
         roverStatus: {
           position: 98382,
@@ -40,26 +38,26 @@ describe("Rover class", function (){
           generatorWatts: 110
           }
         };
-      assert.deepEqual(actual, expected);
+      expect(actual).toEqual(expectedReturn);
 });
   it("responds correctly to mode change command", function (){
     let commands = [new Command("MODE_CHANGE", "LOW_POWER")];
     let message = new Message("mode change command", commands);
     let rover = new Rover(98382);
     let response = rover.receiveMessage(message);
-
-    assert.strictEqual(rover.mode, "LOW_POWER");
-    assert.strictEqual(response.results[0]. completed, true);
+    expect(rover.mode).toEqual('LOW_POWER');
+    expect(response.results[0]).toEqual(({completed: true}))
   });
   it("responds with false completed value when attempting to move in 'LOW_POWER' mode", function() {
         let rover = new Rover(98382);
         let commands = [new Command("MODE_CHANGE", "LOW_POWER"), new Command("MOVE", 30)];
         let message = new Message("test message - move attempt for test 12", commands);
         let response = rover.receiveMessage(message);
-        assert.strictEqual(rover.position, 98382);
-        assert.strictEqual(rover.mode, "LOW_POWER");
-        assert.strictEqual(response.results[1].completed, false);
-        assert.strictEqual(response.results[0]. completed, true);
+        expect(rover.position).toEqual(98382);
+        expect(rover.mode).toEqual('LOW_POWER');
+        expect(response.results[1]).toEqual(({completed: false}));
+        expect(response.results[0]).toEqual(({completed: true}));
+
   });
     it("responds with position for move command", function (){
       let commands = [new Command("MOVE", 150)];
@@ -67,8 +65,8 @@ describe("Rover class", function (){
       let rover = new Rover(98382);
 
       rover.receiveMessage(message);
-  
-      assert.strictEqual(rover.position, 150);
+
+      expect(rover.position).toEqual(150);
     });
   });
 
